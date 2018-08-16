cfbdf5d8cd7a1c9858e230369b6372b7e072788a
unknown <jayng168@gmail.com>
2018-08-16 01:02:26 -0400

    Updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 var winCount = 0;
 var loseCount = 0;
-var guessLeft = 6;
+var guessLeft = 10;
 var playerGuess = [];
-var results = "";
+var results = document.getElementById("results");
 
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -12,32 +12,30 @@ document.onkeyup = function (event) {
 
     if (userGuess == computerGuess) {
         winCount += 1;
-        results = "You got it right!";
+        results.textContent = "You Got It Right!";
     }
     else if (playerGuess.includes(userGuess)) {
         guessLeft -= 1;
-        results = "You lost!";
+        results.textContent = "You Already Guessed That Letter!";
     }
     else {
         guessLeft -= 1;
-        results = "You lost!";
+        results.textContent =  "You Lost!";
         playerGuess.push(userGuess);
     };
 
     if (guessLeft === 0) {
         loseCount += 1;
-        guessLeft = 5;
+        guessLeft = 10;
         playerGuess = [];
     }
     else {
 
     };
 
-    document.getElementById("title").innerHTML = "Welcome To The Psychic Game!";
-    document.getElementById("guess").innerHTML = "Guess what the letter I'm thinking of";
-    document.getElementById("wins").innerHTML = "Wins: " + winCount;
-    document.getElementById("loses").innerHTML = "Loses: " + loseCount;
-    document.getElementById("loses").innerHTML = "Loses: " + loseCount;
-    document.getElementById("guessleft").innerHTML = "Guesses Left: " + guessLeft;
-    document.getElementById("lettersleft").innerHTML = "Your guess so far: " + playerGuess;
+    document.getElementById("wins").innerHTML = winCount;
+    document.getElementById("loses").innerHTML = loseCount;
+    document.getElementById("guessleft").innerHTML = guessLeft;
+    document.getElementById("lettersleft").innerHTML = playerGuess;
 };
+
